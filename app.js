7c85ffb14a0106222a2f6662c0bf05c27dd987d6
GuvenBaris <guven.cakan@affinitybox.com>
2022-11-22 19:56:51 +0300

    static files

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+// Template Engine
+app.set('view engine', 'ejs');
 
+// Middleware
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname,));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
